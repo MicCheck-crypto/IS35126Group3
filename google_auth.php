48fc2097f7e62d169338be2eab9bafdf52681bc7
MicCheck-crypto <miikapeterpalmer@gmail.com>
2026-06-04 23:31:25 +1200

    Update google_auth.php

--- google_auth.php
+++ google_auth.php
@@ -8,7 +8,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 $client = new Google\Client();
 $client->setClientId('332167711514-33fl96ti4tea9qc249kajf09j87jmld4.apps.googleusercontent.com');
 $client->setClientSecret('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');
-$client->setRedirectUri('http://localhost/project/google_callback.php');
+$redirect_uri = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'
+    ? 'https://' . $_SERVER['HTTP_HOST'] . '/google_callback.php'
+    : 'http://' . $_SERVER['HTTP_HOST'] . '/google_callback.php';
+$client->setRedirectUri($redirect_uri);
 $client->addScope('email');
 $client->addScope('profile');
 $client->setAccessType('online');
